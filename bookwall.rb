718eab3ef6eded8b0a54d25bef3c048b4dee08bd
laoar <laoar.shao@gmail.com>
2014-08-10 14:00:32 +0800

    error handle and info
    update with argument error handling and the informations.

--- bookwall.rb
+++ bookwall.rb
@@ -1,11 +1,16 @@
 #!/usr/bin/ruby
 
 # usage : ./bookwall username
+#http://www.douban.com/people/username/
 
-require 'rubygems'
-require 'mechanize'
-require 'nokogiri'  
-require 'open-uri'
+require "mechanize"
+#require 'nokogiri'  
+#require 'open-uri'
+
+if ARGV.size != 1
+	print "usage: ./bookwall username\n"
+	exit -1 
+end 
 
 $name = ARGV[0] 
 $uri = sprintf("http://book.douban.com/people/%s/collect?start=0&sort=time&rating=all&filter=all&mode=grid", $name);
@@ -13,7 +18,13 @@ $uri = sprintf("http://book.douban.com/people/%s/collect?start=0&sort=time&ratin
 agent = Mechanize.new
 page = agent.get($uri)
 
-$book = File.new("bookwall.html", "a+")
+$book = File.new("bookwall.html", "w+")
+if $book == nil
+	exit -1
+end
+$book.printf("<title>%s's Book</title>", $name)
+$book.puts "<body bgcolor=\"#eee\">"
+
 $page = page
 begin 
 	page = agent.click($page)
@@ -26,6 +37,7 @@ begin
 	sleep 3
 	$page = page.links.find {|l| l.text == '后页>'}
 end while $page  
+$book.printf("<hr></p>NOTE: These are the books @%s ever read, listed by the date when it got finished.</p>",$name)
 $book.close
 
 =begin
